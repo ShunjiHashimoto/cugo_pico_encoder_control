b528c90915ca7fbf80eee0a0826d050ff6987b2b
ShunjiHashimoto <h.shunji1206@gmail.com>
2026-03-02 23:30:03 +0900

    test: encoder_count_testのBLDC計測と方向判定を整理
    SPEED-OUTを左右独立カウントに変更し、BLDC時の表示をL/R別に更新
    
    DIR入力の極性を左右個別設定化し、左モータのFWD/REV逆転を修正
    
    DC quadratureとBLDC処理の初期化・割り込み分岐を明確化

--- test/encoder_count_test/encoder_count_test.ino
+++ test/encoder_count_test/encoder_count_test.ino
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 
-// Set to 1 for left encoder, 0 for right encoder.
+// DC mode only: set to 1 for left encoder, 0 for right encoder.
 #ifndef USE_LEFT_ENCODER
 #define USE_LEFT_ENCODER 0
 #endif
@@ -9,7 +9,7 @@
 // 1: BLDC driver HM-5100J (SPEED-OUT)
 // 2: BLDC driver HP-5097 (SPEED-OUT)
 #ifndef DRIVER_TYPE
-#define DRIVER_TYPE 0
+#define DRIVER_TYPE 1
 #endif
 
 #define DRIVER_DC 0
@@ -22,6 +22,10 @@ constexpr uint8_t PIN_ENCODER_R_A = 8;
 constexpr uint8_t PIN_ENCODER_R_B = 9;
 constexpr uint8_t PIN_SPEED_OUT_L = 5;
 constexpr uint8_t PIN_SPEED_OUT_R = 7;
+constexpr uint8_t PIN_DIRECTION_L = 12;
+constexpr uint8_t PIN_DIRECTION_R = 13;
+constexpr bool kDirectionHighIsForwardLeft = false;
+constexpr bool kDirectionHighIsForwardRight = true;
 
 constexpr bool kLeftReverse = false;
 constexpr bool kRightReverse = true;
@@ -40,6 +44,14 @@ struct EncoderState {
 };
 
 EncoderState encoder;
+volatile long bldc_count_left = 0;
+volatile long bldc_count_right = 0;
+
+int8_t readDirectionSign(uint8_t pin_direction, bool high_is_forward) {
+  bool high = (digitalRead(pin_direction) == HIGH);
+  bool forward = high_is_forward ? high : !high;
+  return forward ? 1 : -1;
+}
 
 void updateQuadratureEncoder() {
   bool a_state = digitalRead(encoder.pin_a);
@@ -67,48 +79,35 @@ void updateQuadratureEncoder() {
 }
 
 void encoderHandler() {
-  if (encoder.use_quadrature) {
-    updateQuadratureEncoder();
-  } else {
-    encoder.count++;
-  }
+  updateQuadratureEncoder();
+}
+
+void speedOutLeftHandler() {
+  bldc_count_left += readDirectionSign(PIN_DIRECTION_L, kDirectionHighIsForwardLeft);
+}
+
+void speedOutRightHandler() {
+  bldc_count_right += readDirectionSign(PIN_DIRECTION_R, kDirectionHighIsForwardRight);
 }
 
 void setup() {
   Serial.begin(kBaud);
 
-#if USE_LEFT_ENCODER
-  Serial.println("Encoder: LEFT");
-#else
-  Serial.println("Encoder: RIGHT");
-#endif
-
   encoder.use_quadrature = (DRIVER_TYPE == DRIVER_DC);
-
-#if USE_LEFT_ENCODER
-#if DRIVER_TYPE == DRIVER_DC
-  encoder.pin_a = PIN_ENCODER_L_A;
-  encoder.pin_b = PIN_ENCODER_L_B;
-  encoder.reverse = kLeftReverse;
-#else
-  encoder.pin_a = PIN_SPEED_OUT_L;
-  encoder.pin_b = 255;
-  encoder.reverse = false;
-#endif
-#else
-#if DRIVER_TYPE == DRIVER_DC
-  encoder.pin_a = PIN_ENCODER_R_A;
-  encoder.pin_b = PIN_ENCODER_R_B;
-  encoder.reverse = kRightReverse;
-#else
-  encoder.pin_a = PIN_SPEED_OUT_R;
-  encoder.pin_b = 255;
-  encoder.reverse = false;
-#endif
-#endif
 
 #if DRIVER_TYPE == DRIVER_DC
   Serial.println("Driver: DC (quadrature)");
+  #if USE_LEFT_ENCODER
+    Serial.println("Encoder: LEFT");
+    encoder.pin_a = PIN_ENCODER_L_A;
+    encoder.pin_b = PIN_ENCODER_L_B;
+    encoder.reverse = kLeftReverse;
+  #else
+    Serial.println("Encoder: RIGHT");
+    encoder.pin_a = PIN_ENCODER_R_A;
+    encoder.pin_b = PIN_ENCODER_R_B;
+    encoder.reverse = kRightReverse;
+  #endif
 #elif DRIVER_TYPE == DRIVER_BLDC_HM5100J
   Serial.println("Driver: BLDC HM-5100J (SPEED-OUT)");
 #elif DRIVER_TYPE == DRIVER_BLDC_HP5097
@@ -117,44 +116,79 @@ void setup() {
 #error "Unsupported DRIVER_TYPE. Use 0, 1, or 2."
 #endif
 
-  pinMode(encoder.pin_a, INPUT_PULLUP);
   if (encoder.use_quadrature) {
+    pinMode(encoder.pin_a, INPUT_PULLUP);
     pinMode(encoder.pin_b, INPUT_PULLUP);
     uint8_t initial_a = digitalRead(encoder.pin_a);
     uint8_t initial_b = digitalRead(encoder.pin_b);
     encoder.last_state = static_cast<uint8_t>((initial_a << 1) | (initial_b & 0x01));
     attachInterrupt(digitalPinToInterrupt(encoder.pin_a), encoderHandler, CHANGE);
     attachInterrupt(digitalPinToInterrupt(encoder.pin_b), encoderHandler, CHANGE);
+    encoder.count = 0;
   } else {
-    encoder.last_state = 0;
-    attachInterrupt(digitalPinToInterrupt(encoder.pin_a), encoderHandler, RISING);
+    pinMode(PIN_SPEED_OUT_L, INPUT_PULLUP);
+    pinMode(PIN_SPEED_OUT_R, INPUT_PULLUP);
+    pinMode(PIN_DIRECTION_L, INPUT_PULLUP);
+    pinMode(PIN_DIRECTION_R, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(PIN_SPEED_OUT_L), speedOutLeftHandler, RISING);
+    attachInterrupt(digitalPinToInterrupt(PIN_SPEED_OUT_R), speedOutRightHandler, RISING);
     Serial.print("SPEED-OUT pulses/rev: ");
     Serial.println(kSpeedOutPulsesPerRev);
+    Serial.println("Direction pins: LEFT=GPIO12, RIGHT=GPIO13");
+    bldc_count_left = 0;
+    bldc_count_right = 0;
   }
-  encoder.count = 0;
 }
 
 void loop() {
   static unsigned long last_report = 0;
-  static long last_count = 0;
+  static long last_count_left = 0;
+  static long last_count_right = 0;
   if (millis() - last_report >= kReportMs) {
     last_report = millis();
-    long count_snapshot;
-    noInterrupts();
-    count_snapshot = encoder.count;
-    interrupts();
-    Serial.print("count=");
-    Serial.print(count_snapshot);
-    if (!encoder.use_quadrature) {
-      long diff = count_snapshot - last_count;
-      float rev = static_cast<float>(diff) / static_cast<float>(kSpeedOutPulsesPerRev);
-      float rpm = rev * (60000.0f / static_cast<float>(kReportMs));
-      Serial.print(", diff=");
-      Serial.print(diff);
-      Serial.print(", rpm_est=");
-      Serial.print(rpm, 2);
-      last_count = count_snapshot;
+    if (encoder.use_quadrature) {
+      long count_snapshot;
+      noInterrupts();
+      count_snapshot = encoder.count;
+      interrupts();
+      Serial.print("count=");
+      Serial.println(count_snapshot);
+    } else {
+      long count_left_snapshot;
+      long count_right_snapshot;
+      noInterrupts();
+      count_left_snapshot = bldc_count_left;
+      count_right_snapshot = bldc_count_right;
+      interrupts();
+
+      long diff_left = count_left_snapshot - last_count_left;
+      long diff_right = count_right_snapshot - last_count_right;
+      float rev_left = static_cast<float>(diff_left) / static_cast<float>(kSpeedOutPulsesPerRev);
+      float rev_right = static_cast<float>(diff_right) / static_cast<float>(kSpeedOutPulsesPerRev);
+      float rpm_left = rev_left * (60000.0f / static_cast<float>(kReportMs));
+      float rpm_right = rev_right * (60000.0f / static_cast<float>(kReportMs));
+      bool forward_left = (readDirectionSign(PIN_DIRECTION_L, kDirectionHighIsForwardLeft) > 0);
+      bool forward_right = (readDirectionSign(PIN_DIRECTION_R, kDirectionHighIsForwardRight) > 0);
+
+      Serial.print("L:count=");
+      Serial.print(count_left_snapshot);
+      Serial.print(",diff=");
+      Serial.print(diff_left);
+      Serial.print(",rpm_est=");
+      Serial.print(rpm_left, 2);
+      Serial.print(",dir=");
+      Serial.print(forward_left ? "FWD" : "REV");
+      Serial.print(" | R:count=");
+      Serial.print(count_right_snapshot);
+      Serial.print(",diff=");
+      Serial.print(diff_right);
+      Serial.print(",rpm_est=");
+      Serial.print(rpm_right, 2);
+      Serial.print(",dir=");
+      Serial.println(forward_right ? "FWD" : "REV");
+
+      last_count_left = count_left_snapshot;
+      last_count_right = count_right_snapshot;
     }
-    Serial.println();
   }
 }
